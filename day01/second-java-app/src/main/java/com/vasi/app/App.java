bf17e25b5134e205d8cd5eeeadc38d1eac22c232
sfjbs <vashishtha.gautam09@gmail.com>
2023-01-12 06:59:19 +0000

    Adding greeting class: adding hi-method to it

--- day01/second-java-app/src/main/java/com/vasi/app/App.java
+++ day01/second-java-app/src/main/java/com/vasi/app/App.java
@@ -11,6 +11,7 @@ public class App {
         String hello = greeting.sayHello();
         String welcome = greeting.sayWelcome();
         String bye = greeting.sayBye();
+        System.out.println(hi);
         System.out.println(hello);
         System.out.println(welcome);
         System.out.println(bye);
