a6a349ad4dde35ec8629e2c68add6f3908f37b7f
sfjbs <vashishtha.gautam09@gmail.com>
2023-01-12 06:57:57 +0000

    Adding greeting class

--- day01/second-java-app/src/main/java/com/vasi/app/App.java
+++ day01/second-java-app/src/main/java/com/vasi/app/App.java
@@ -4,10 +4,15 @@ package com.vasi.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Greeting greeting = new Greeting();
+        String hi = greeting.sayHi();
+        String hello = greeting.sayHello();
+        String welcome = greeting.sayWelcome();
+        String bye = greeting.sayBye();
+        System.out.println(hello);
+        System.out.println(welcome);
+        System.out.println(bye);
     }
 }
---
+++ day01/second-java-app/src/main/java/com/vasi/app/Greeting.java
@@ -0,0 +1,11 @@
+package com.vasi.app;
+
+public class Greeting {
+
+    public String sayHi(){
+       return "Hi from greeting";
+    }
+    public String sayHello(){return "Hello from greeting";}
+    public String sayWelcome(){return "Welcome from greeting";}
+    public String sayBye(){return "Bye from Greeting";}
+}
