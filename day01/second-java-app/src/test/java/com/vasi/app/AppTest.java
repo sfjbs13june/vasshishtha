ebb2fe6402111af8d626609b170eb40c9e7abeca
sfjbs <vashishtha.gautam09@gmail.com>
2023-01-12 07:25:59 +0000

    adding app test

--- day01/second-java-app/src/test/java/com/vasi/app/AppTest.java
+++ day01/second-java-app/src/test/java/com/vasi/app/AppTest.java
@@ -1,38 +1,35 @@
 package com.vasi.app;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+    @Test
+    public void testSayHi(){
+        String expectedResult= "Hi from greeting";
+        Greeting greeting =new Greeting();
+        String result= greeting.sayHi();
+        Assert.assertEquals(expectedResult,result);
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public void testSayHello(){
+        String expectedResult= "Hello from greeting";
+        Greeting greeting =new Greeting();
+        String hello_result= greeting.sayHello();
+        Assert.assertEquals(expectedResult,hello_result);
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testSayWelcome(){
+        String expectedResult= "Welcome from greeting";
+        Greeting greeting =new Greeting();
+        String welcome_result= greeting.sayWelcome();
+        Assert.assertEquals(expectedResult,welcome_result);
+    }
+    public void testSayBye(){
+        String expectedResult= "bye from greeting";
+        Greeting greeting =new Greeting();
+        String bye_result= greeting.sayBye();
+        Assert.assertEquals(expectedResult,bye_result);
     }
 }
