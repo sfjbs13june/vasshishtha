14317c2a9778ccfe949465bb43ce65ae2e1aaddd
sfjbs <vashishtha.gautam09@gmail.com>
2023-01-13 06:21:00 +0000

    adding dependencies_second-spring-project

---
+++ day02/second-spring-project/README.md
@@ -0,0 +1,8 @@
+# day 2
+# Create Springboot project
+https://start.spring.io/
+
+# Build project
+mvn clean install
+# Run the application
+mvn spring-boot:run
---
+++ day02/second-spring-project/src/test/java/com/vasi/app/SecondSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
