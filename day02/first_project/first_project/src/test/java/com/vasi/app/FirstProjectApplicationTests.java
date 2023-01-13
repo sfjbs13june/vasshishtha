5d9ee98d9277c9ba7ba907055d2745df1e33d3fe
sfjbs <vashishtha.gautam09@gmail.com>
2023-01-13 05:26:47 +0000

    first_spring_project_final

---
+++ day02/first_project/first_project/src/test/java/com/vasi/app/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
